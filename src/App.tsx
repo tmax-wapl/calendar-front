5ff6fc901896cd41293e210201f46a7a33412d34
soohyun <soohyun_kim2@tmax.co.kr>
2022-11-02 13:29:30 +0900

    project settings

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+const App: React.FC = () => {
+  return <div>calendar app</div>;
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+
+const { CleanWebpackPlugin } = require('clean-webpack-plugin'); //추가
+const HtmlWebpackPlugin = require('html-webpack-plugin'); //추가
+
+const mode = process.env.NODE_ENV || 'development'; //1
+
+module.exports = {
+  mode,
+  entry: './src/index.tsx',
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.min.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        use: ['babel-loader', 'ts-loader'],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(), // 웹팩 실행시마다 dist 폴더 정리
+    new HtmlWebpackPlugin({
+      //index.html 자동 생성되도록 template 옵션 설정
+      template: './public/index.html',
+      templateParameters: {
+        env: process.env.NODE_ENV === 'production' ? '' : '[DEV]',
+      },
+      minify:
+        process.env.NODE_ENV === 'production'
+          ? {
+              collapseWhitespace: true,
+              removeComments: true,
+            }
+          : false,
+    }),
+  ],
+};
