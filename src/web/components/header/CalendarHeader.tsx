b71c119d11fd09a91c962390ac39cf7e9d7b901a
soohyun <soohyun_kim2@tmax.co.kr>
2022-11-08 16:20:11 +0900

    feat(note): header 컴포넌트 및 mainCalendar 추가

--- src/web/components/header/CalendarHeader.tsx
+++ src/web/components/header/CalendarHeader.tsx
@@ -7,23 +7,37 @@ import {
   TodayButton,
   ViewSelect,
 } from './ClaendarHeader.style';
+import React from 'react';
 
 const CalendarHeader: React.FC = () => {
+  const handlePrevMonth = () => {
+    console.log('prev');
+  };
+
+  const handleNextMonth = () => {
+    console.log('next');
+  };
+
+  const handleViewChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const { value } = e.target;
+    console.log(value);
+  };
+
   return (
     <CalendarHeaderContainer>
       <div style={{ display: 'flex' }}>
         <DateButton>2022.11</DateButton>
         <ButtonWrapper>
-          <PrevButton />
-          <NextButton />
+          <PrevButton onClick={handlePrevMonth} />
+          <NextButton onClick={handleNextMonth} />
         </ButtonWrapper>
         <TodayButton>오늘</TodayButton>
       </div>
       <div style={{ display: 'flex' }}>
-        <ViewSelect>
-          <option>월</option>
-          <option>주</option>
-          <option>일</option>
+        <ViewSelect onChange={handleViewChange}>
+          <option value="dayGridMonth">월</option>
+          <option value="timeGridWeek">주</option>
+          <option value="timeGridDay">일</option>
         </ViewSelect>
       </div>
     </CalendarHeaderContainer>
