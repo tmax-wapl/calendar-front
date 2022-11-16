a4df072f1c18277ace1527b9eff0e599b6c19d66
DaeunOh <dev.carrotoh@gmail.com>
2022-11-16 16:01:25 +0900

    feat(calendar): EventBar 컴포넌트 추가

---
+++ src/web/components/EventBar.tsx
@@ -0,0 +1,44 @@
+import { Icon } from '@wapl/ui';
+import { EventBarContainer, IconButton, EventBarTitle } from './EventBar.style';
+
+interface EventBarButton {
+  action: 'prev' | 'share' | 'edit' | 'delete';
+  onClick: () => void;
+}
+
+interface Props {
+  title?: string;
+  leftSide?: EventBarButton[];
+  rightSide?: EventBarButton[];
+}
+
+const EventBar = ({ title = 'title', leftSide = [], rightSide = [] }: Props) => {
+  const ButtonIcon = {
+    prev: <Icon.ArrowBackLine color="#202124" width={20} height={20} />,
+    share: <Icon.ShareLine color="#202124" width={20} height={20} />,
+    edit: <Icon.EditLine color="#202124" width={20} height={20} />,
+    delete: <Icon.DeleteLine color="#202124" width={20} height={20} />,
+  };
+
+  const Buttons = ({ buttons }: { buttons: EventBarButton[] }) => {
+    return (
+      <>
+        {buttons.map(button => (
+          <IconButton key={button.action} onClick={button.onClick}>
+            {ButtonIcon[button.action]}
+          </IconButton>
+        ))}
+      </>
+    );
+  };
+
+  return (
+    <EventBarContainer>
+      <Buttons buttons={leftSide} />
+      <EventBarTitle>{title}</EventBarTitle>
+      <Buttons buttons={rightSide} />
+    </EventBarContainer>
+  );
+};
+
+export default EventBar;
