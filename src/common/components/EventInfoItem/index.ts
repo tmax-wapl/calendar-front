c38ce2d5be9f3caec2dc8854e7cc16ad2ff52fde
DaeunOh <dev.carrotoh@gmail.com>
2022-11-16 17:25:21 +0900

    chore(calendar): EventInfoItem index 파일 및 임시 EventDTO interface 추가

---
+++ src/common/components/EventInfoItem/index.ts
@@ -0,0 +1,7 @@
+import Participants from './Participants';
+import Location from './Location';
+import Notifications from './Notifications';
+import Description from './Description';
+import Attachments from './Attachments';
+
+export { Participants, Location, Notifications, Description, Attachments };
