fc082d6309f81967849e0afbb67a11f255456fa2
DaeunOh <dev.carrotoh@gmail.com>
2022-12-02 21:22:51 +0900

    fix(calendar): Location/Description 내부 state 사용 조건 변경 및 React.memo 적용

--- src/common/components/EventInfoItem/Description.tsx
+++ src/common/components/EventInfoItem/Description.tsx
@@ -1,18 +1,20 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { Icon } from '@wapl/ui';
 import { DescriptionContainer, ItemTitleContainer, DescriptionTextarea } from './Description.style';
 
 interface Props {
-  description: string;
+  description?: string;
+  onChange?: (value: { description: string }) => void;
   editable?: boolean;
 }
 
-const Description = ({ description, editable = false }: Props) => {
-  const [data, setData] = useState<typeof description>(description);
+const Description = ({ description = '', onChange, editable = false }: Props) => {
+  const [value, setValue] = useState<typeof description>('');
 
   // TODO: editor로 변경
   const handleTextareaChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    setData(e.target.value);
+    if (onChange) onChange({ description: e.target.value });
+    setValue(e.target.value);
   };
 
   return (
@@ -21,9 +23,14 @@ const Description = ({ description, editable = false }: Props) => {
         <Icon.EditingTextalignLeftLine className="mr-8" color="#202124" width={20} height={20} />
         설명
       </ItemTitleContainer>
-      <DescriptionTextarea value={data} placeholder="설명 추가" onChange={handleTextareaChange} readOnly={!editable} />
+      <DescriptionTextarea
+        value={description || value}
+        placeholder="설명 추가"
+        onChange={handleTextareaChange}
+        readOnly={!editable}
+      />
     </DescriptionContainer>
   );
 };
 
-export default Description;
+export default React.memo(Description, (prev, next) => prev.description === next.description);
--- src/common/components/EventInfoItem/Location.tsx
+++ src/common/components/EventInfoItem/Location.tsx
@@ -1,25 +1,32 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { Icon } from '@wapl/ui';
 import { LocationContainer, LocationInput } from './Location.style';
 
 interface Props {
-  location: string;
+  location?: string;
+  onChange?: (value: { location: string }) => void;
   editable?: boolean;
 }
 
-const Location = ({ location, editable = false }: Props) => {
-  const [data, setData] = useState<typeof location>(location);
+const Location = ({ location, onChange, editable = false }: Props) => {
+  const [value, setValue] = useState<typeof location>('');
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setData(e.target.value);
+    if (onChange) onChange({ location: e.target.value });
+    setValue(e.target.value);
   };
 
   return (
     <LocationContainer>
       <Icon.LocationLine className="mr-8" color="#202124" width={20} height={20} />
-      <LocationInput value={data} placeholder="장소 추가" onChange={handleInputChange} readOnly={!editable} />
+      <LocationInput
+        value={location || value}
+        placeholder="장소 추가"
+        onChange={handleInputChange}
+        readOnly={!editable}
+      />
     </LocationContainer>
   );
 };
 
-export default Location;
+export default React.memo(Location, (prev, next) => prev.location === next.location);
