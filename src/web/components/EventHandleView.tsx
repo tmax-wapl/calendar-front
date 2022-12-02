efecd74043fe7929fb98119f5ec150bfb75beb23
DaeunOh <dev.carrotoh@gmail.com>
2022-12-02 21:24:18 +0900

    feat(calendar): 일정 생성/수정 컴포넌트(EventHandleView) 추가

---
+++ src/web/components/EventHandleView.tsx
@@ -0,0 +1,93 @@
+import { useState } from 'react';
+import { DateTime } from 'luxon';
+import { EventModel } from '@common/constants/interfaces';
+import { EventHandleViewContainer, EventHandleContainer } from './EventHandleView.style';
+import EventBar from './EventBar';
+import {
+  EventTitle,
+  EventDate,
+  RepeatInfo,
+  Participants,
+  Location,
+  Notifications,
+  Description,
+  Attachments,
+} from '@/common/components/EventInfoItem';
+
+interface Props {
+  action: 'create' | 'edit';
+}
+
+const EventHandleView = ({ action }: Props) => {
+  const [event, setEvent] = useState<Partial<EventModel>>({}); // [임시] for create
+  // const [event, setEvent] = useState<Partial<EventModel>>({
+  //   importance: false,
+  //   title: '일정 제목 일정 제목일정 제목일정 제목일정 제목ㅇㄹㄴㄹㄴㅇㄹ',
+  //   allDay: false,
+  //   startDate: DateTime.fromMillis(1630627200000), // 2021-09-03T09:00:00
+  //   startTime: DateTime.fromMillis(1630627200000),
+  //   endDate: DateTime.fromMillis(1630629000000), // 2021-09-03T09:30:00
+  //   endTime: DateTime.fromMillis(1630629000000),
+  //   repeatTime: '1',
+  //   repeatUnit: 'w',
+  //   repeatEndDate: DateTime.fromMillis(1630627200000),
+  //   repeatDays: [true, true, true, true, true, true, false],
+  //   color: '#FF46B5',
+  //   participants: [
+  //     { id: 1, name: '이광옹' },
+  //     { id: 2, name: '안순범' },
+  //     { id: 3, name: '솜다연' },
+  //   ],
+  //   location: 'Tmax 오리 연구소',
+  //   notifications: ['0', '5 m', '30 m', '1 h', '2 d'],
+  //   description:
+  //     '설커다란 대한 청춘의 뿐이다. 너의 소리다.이것은 구할 곳이 듣기만 있는 품으며, 얼마나 사랑의 황금시대다. 군영과 붙잡아 인생에 ',
+  //   attachments: [
+  //     { id: 1, name: 'Txt', extension: 'png', size: 80000 },
+  //     { id: 2, name: 'Txt', extension: 'png', size: 80000 },
+  //   ],
+  // }); // [임시] for edit
+
+  return (
+    <EventHandleViewContainer>
+      <EventBar
+        title={action === 'create' ? '새 일정' : '일정 수정'}
+        leftSide={[{ action: 'close', onClick: () => console.log('close') }]}
+      />
+      <EventHandleContainer>
+        <EventTitle title={event.title} importance={event.importance} />
+        <EventDate
+          allDay={event.allDay}
+          startDate={event.startDate || DateTime.now()}
+          startTime={event.startTime || DateTime.now()}
+          endDate={event.endDate || DateTime.now()}
+          endTime={event.endTime || DateTime.now().plus({ minutes: 30 })}
+          onChange={value => setEvent(prev => ({ ...prev, ...value }))}
+        />
+        <RepeatInfo
+          repeatTime={event.repeatTime}
+          repeatUnit={event.repeatUnit}
+          repeatEndDate={event.repeatEndDate}
+          defaultEndDate={event.startDate?.plus({ years: 1 })}
+          repeatDays={event.repeatDays}
+          onChange={value => setEvent(prev => ({ ...prev, ...value }))}
+        />
+        <Participants participants={event.participants} editable />
+        <Location location={event.location} onChange={value => setEvent(prev => ({ ...prev, ...value }))} editable />
+        <Notifications
+          notifications={event.notifications}
+          onChange={value => setEvent(prev => ({ ...prev, ...value }))}
+          editable
+        />
+        <Description
+          description={event.description}
+          onChange={value => setEvent(prev => ({ ...prev, ...value }))}
+          editable
+        />
+        <Attachments attachments={event.attachments} editable />
+      </EventHandleContainer>
+    </EventHandleViewContainer>
+  );
+};
+
+export default EventHandleView;
--- src/web/index.tsx
+++ src/web/index.tsx
@@ -4,6 +4,7 @@ import Calendar from '@wcomponents/CalendarView';
 import Share from '@wcomponents/ShareView';
 import EventList from '@wcomponents/EventListView';
 import Detail from '@wcomponents/EventDetailView';
+import EventHandleView from '@wcomponents/EventHandleView';
 import { ROUTES } from '@constants/routes';
 
 const WebApp: React.FC = () => {
@@ -35,7 +36,7 @@ const WebApp: React.FC = () => {
         <Route element={<CalendarLayout />}>
           <Route path="*" element={<Navigate to={ROUTES.PATH_MAIN} replace />} />
           <Route path={ROUTES.MAIN} element={<Calendar />}>
-            <Route path={ROUTES.CREATE} element={<EventList />} />
+            <Route path={ROUTES.CREATE} element={<EventHandleView action="create" />} />
             <Route path={ROUTES.DETAIL} element={<Detail />} />
           </Route>
           <Route path={ROUTES.SHARE} element={<Share />} />
