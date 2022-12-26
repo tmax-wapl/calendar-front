2faab9018c629082cc7a7e5652400526333a5cb3
DaeunOh <dev.carrotoh@gmail.com>
2022-12-26 16:10:00 +0900

    feat(calendar): 시작일/마감일에 사용되는 DatePicker/TimePicker 통합 컴포넌트(EventDateItem) 생성

---
+++ src/common/components/EventInfoItem/EventDateItem.tsx
@@ -0,0 +1,64 @@
+import { useState } from 'react';
+import { DateTime } from 'luxon';
+import { Icon } from '@wapl/ui';
+import {
+  EventDateItemContainer,
+  PickerContainer,
+  DateWrapper,
+  DatePickerWrapper,
+  TimeWrapper,
+  TimePickerWrapper,
+} from './EventDateItem.style';
+import DatePicker from '../DatePicker/DatePicker';
+import TimePicker from '../TimePicker/TimePicker';
+
+interface Props {
+  title?: string;
+  date: DateTime;
+  allDay?: boolean;
+  onChange?: (date: DateTime) => void;
+}
+
+const EventDateItem = ({ title, date, allDay, onChange }: Props) => {
+  const [isDatePickerOpen, setIsDatePickerOpen] = useState<boolean>(false);
+  const [isTimePickerOpen, setIsTimePickerOpen] = useState<boolean>(false);
+
+  const handleDateClick = () => {
+    setIsDatePickerOpen(prev => !prev);
+  };
+
+  const handleTimeClick = () => {
+    setIsTimePickerOpen(prev => !prev);
+  };
+
+  return (
+    <EventDateItemContainer>
+      {title}
+      <PickerContainer>
+        <DateWrapper className={`${isDatePickerOpen ? 'selected' : ''}`} onClick={handleDateClick}>
+          {date.toFormat('yyyy.LL.dd')}
+          <Icon.CalendarLine className="ml-8" color="#202124" width={16} height={16} />
+        </DateWrapper>
+        {isDatePickerOpen && (
+          <DatePickerWrapper>
+            <DatePicker size={0.85} date={date} onDateClick={onChange} onOutsideClick={handleDateClick} />
+          </DatePickerWrapper>
+        )}
+      </PickerContainer>
+      {!allDay && (
+        <PickerContainer>
+          <TimeWrapper className={`${isTimePickerOpen ? 'selected' : ''}`} onClick={handleTimeClick}>
+            {date.toFormat('a h:mm', { locale: 'ko' })}
+          </TimeWrapper>
+          {isTimePickerOpen && (
+            <TimePickerWrapper>
+              <TimePicker value={date} onChange={onChange} onOutsideClick={handleTimeClick} />
+            </TimePickerWrapper>
+          )}
+        </PickerContainer>
+      )}
+    </EventDateItemContainer>
+  );
+};
+
+export default EventDateItem;
