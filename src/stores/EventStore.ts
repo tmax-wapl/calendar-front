6a63d605c08482f7b233c5820242068bc5d50cb3
soohyun <soohyun_kim2@tmax.co.kr>
2023-01-03 17:32:10 +0900

    feat(calendar): route detail params id 값 store 변수로 변경

--- src/stores/EventStore.ts
+++ src/stores/EventStore.ts
@@ -1,14 +1,19 @@
 import RootStore from './RootStore';
 import EventRepo from './repository/EventRepo';
 import { EventDTO } from '@/common/constants/interfaces';
+import { makeObservable, observable } from 'mobx';
 
 export default class EventStore {
   rootStore: RootStore;
   repo: EventRepo;
+  eventId: number = null;
 
   constructor(rootStore: RootStore) {
     this.rootStore = rootStore;
     this.repo = new EventRepo();
+    makeObservable(this, {
+      eventId: observable,
+    });
   }
 
   async getEventInfo(eventId: number) {
