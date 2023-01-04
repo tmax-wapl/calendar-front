73af4807e38a4cae8e9874d9f047c43af9c39bd6
DaeunOh <dev.carrotoh@gmail.com>
2023-01-04 11:46:37 +0900

    feat(calendar): EventModel 추가 및 스프링 dto에 맞게 dto 일부 변경

--- src/common/constants/interfaces.ts
+++ src/common/constants/interfaces.ts
@@ -1,46 +1,17 @@
-import { DateTime } from 'luxon';
-
 export interface EventDTO {
   id: number;
   calId: number;
+  calName: string;
   color: string;
   importance: boolean;
-  name: string;
+  title: string;
   allDay: boolean;
-  startDate: string;
-  endDate?: string;
+  start: string;
+  end: string;
   modDate?: string;
   modUserId?: number;
   regDate?: string;
   regUserId?: number;
-  calendarName: string;
-  rrule?: string;
-  alarmList?: AlarmDTO[];
-  exceptionList?: ExceptionDTO[];
-  location?: string;
-  description?: string;
-  repeatEndDate?: string;
-  repeatStartDate?: string;
-  repeatgroupId?: number;
-  participants?: any[];
-  attachments?: any[];
-  notifications?: any[];
-}
-
-export interface EventModel {
-  id: number;
-  calId: number;
-  color: string;
-  importance: boolean;
-  name: string;
-  allDay: boolean;
-  startDate: string;
-  endDate?: string;
-  modDate?: string;
-  modUserId?: number;
-  regDate?: string;
-  regUserId?: number;
-  calendarName: string;
   rrule?: string;
   alarmList?: AlarmDTO[];
   exceptionList?: ExceptionDTO[];
@@ -86,6 +57,7 @@ export interface CalendarPatchDTO {
 export interface EventRangeDTO {
   allday: string;
   calId: number;
+  calName: string;
   color: string;
   endDate: string;
   id: number;
