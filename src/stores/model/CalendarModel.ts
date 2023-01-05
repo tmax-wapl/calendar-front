92422acb98b871fe35ff90ef938f0a005d320e7c
hyojeong_kim <hyojeong_kim@tmax.co.kr>
2023-01-05 16:16:29 +0900

    feat(calendar): CalendarModel 추가 및 dto 변경

---
+++ src/stores/model/CalendarModel.ts
@@ -0,0 +1,69 @@
+import { CalendarDTO, EventRangeDTO } from '@constants/interfaces';
+
+export class CalendarModel {
+  dto: Partial<CalendarDTO>;
+
+  constructor(dto: Partial<CalendarDTO>) {
+    this.dto = dto;
+  }
+
+  get id(): number {
+    return this.dto.id;
+  }
+
+  get name(): string {
+    return this.dto.name;
+  }
+
+  set name(name: string) {
+    this.dto.name = name;
+  }
+
+  get color(): string {
+    return this.dto.color;
+  }
+
+  set color(color: string) {
+    this.dto.color = color;
+  }
+
+  get checkFlag(): boolean {
+    return this.dto.checkFlag;
+  }
+
+  set checkFlag(value: boolean) {
+    this.dto.checkFlag = value;
+  }
+
+  get mainFlag(): boolean {
+    return this.dto.mainFlag;
+  }
+
+  get eventList(): EventRangeDTO[] {
+    return this.dto.eventList;
+  }
+
+  get modDate(): string {
+    return this.dto.modDate;
+  }
+
+  get modUserId(): number {
+    return this.dto.modUserId;
+  }
+
+  get regDate(): string {
+    return this.dto.regDate;
+  }
+
+  get regUserId(): number {
+    return this.dto.regUserId;
+  }
+
+  get type(): string {
+    return this.dto.type;
+  }
+
+  get url(): string {
+    return this.dto.url;
+  }
+}
