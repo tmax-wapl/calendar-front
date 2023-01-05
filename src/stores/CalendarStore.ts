b1281e50bbc53c3510f9d3ccb7bddb7ab5127a7c
hyojeong_kim <hyojeong_kim@tmax.co.kr>
2023-01-05 16:35:02 +0900

    feat(calendar): 유저별 캘린더 리스트 get 서비스 추가

--- src/stores/CalendarStore.ts
+++ src/stores/CalendarStore.ts
@@ -1,18 +1,30 @@
+import { action, makeObservable, observable } from 'mobx';
 import RootStore from './RootStore';
 import CalendarRepo from './repository/CalendarRepo';
+import { CalendarModel } from './model/CalendarModel';
 import { CalendarDTO, CalendarPatchDTO } from '@/common/constants/interfaces';
 
 export default class CalendarStore {
   rootStore: RootStore;
   repo: CalendarRepo;
+  calendarList: Array<CalendarModel> = null;
 
   constructor(rootStore: RootStore) {
     this.rootStore = rootStore;
     this.repo = new CalendarRepo();
+    makeObservable(this, {
+      calendarList: observable,
+      setCalendarList: action,
+    });
   }
 
   async createCalendar(dto: CalendarDTO) {
     const res = await this.repo.calendarCreate(dto);
+    return res;
+  }
+
+  async getUserCalendars(userId: number, start: string, end: string) {
+    const res = await this.repo.getUserCalendars(userId, start, end);
     return res;
   }
 
@@ -29,5 +41,9 @@ export default class CalendarStore {
   async calendarDelete(calId: number) {
     const res = await this.repo.calendarDelete(calId);
     return res;
+  }
+
+  setCalendarList(list: Array<CalendarModel>) {
+    this.calendarList = list;
   }
 }
