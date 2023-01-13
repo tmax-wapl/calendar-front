775c91b11c5e06f3cdfdd7266f016c5fc67ab37c
soohyun <soohyun_kim2@tmax.co.kr>
2023-01-13 14:53:53 +0900

    chore(calendar): develop과 production 에 따라 publicPath 분기 수정

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].js',
-    publicPath: '.',
+    publicPath: mode === 'development' ? '/' : '.',
   },
   module: {
     rules: [
