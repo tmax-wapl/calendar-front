33c175fe739ca999eb702a8a4feee712a4585741
hyojeong_kim <hyojeong_kim@tmax.co.kr>
2023-01-19 11:07:08 +0900

    fix(calendar): 미니캘린더로 공휴일이 있는 날짜 이동 시, 공휴일이 안보이는 이슈

--- src/stores/CalendarStore.ts
+++ src/stores/CalendarStore.ts
@@ -24,6 +24,7 @@ export default class CalendarStore {
       setCalendarList: action,
       eventList: observable,
       setEventList: action,
+      holidayList: observable,
       appendEventList: action,
       updateEventList: action,
       filterEventList: action,
