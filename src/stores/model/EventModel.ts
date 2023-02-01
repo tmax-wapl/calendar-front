d9efcc7dce21bae091303775e2af65a8978a3d7d
soohyun <soohyun_kim2@tmax.co.kr>
2023-02-01 10:34:52 +0900

    fix(calendar): rrule 및 rruleset 판단 여부 로직 수정

--- src/stores/model/EventModel.ts
+++ src/stores/model/EventModel.ts
@@ -2,7 +2,7 @@ import { makeAutoObservable } from 'mobx';
 import { DateTime } from 'luxon';
 import { rrulestr, RRule, Options, RRuleSet } from 'rrule';
 import { EventDTO, AlarmDTO, ExceptionDTO } from '@constants/interfaces';
-import { toISO } from '@/utils';
+import { isRRule, toISO } from '@/utils';
 
 export class EventModel {
   dto: Partial<EventDTO>;
@@ -107,10 +107,11 @@ export class EventModel {
   }
 
   get rrule(): Partial<Options> {
+    const rruleStr = rrulestr(this.dto.rrule);
     return this.dto.rrule
-      ? Object.keys(rrulestr(this.dto.rrule).origOptions).length !== 0
-        ? rrulestr(this.dto.rrule).origOptions
-        : (rrulestr(this.dto.rrule) as RRuleSet)._rrule[0].origOptions
+      ? isRRule(rruleStr)
+        ? rruleStr.origOptions
+        : (rruleStr as RRuleSet)._rrule[0].origOptions
       : undefined;
   }
 
