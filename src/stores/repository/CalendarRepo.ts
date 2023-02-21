86a5ea2708d1dc993ed4d42636ddb438e7e7f487
hyojeong_kim <hyojeong_kim@tmax.co.kr>
2023-02-21 17:46:33 +0900

    fix(calendar): 구독 캘린더 추가 중복 시, 팝업 뜨도록 수정

--- src/stores/repository/CalendarRepo.ts
+++ src/stores/repository/CalendarRepo.ts
@@ -9,7 +9,7 @@ export default class CalendarRepo {
       return response;
     } catch (e) {
       if (e instanceof HTTPError) {
-        throw Error(JSON.stringify(e));
+        throw e;
       }
     }
   }
