ced0dbcb9a9a13ad92b94f3220b24b4d2d46b3f4
hyojeong_kim <hyojeong_kim@tmax.co.kr>
2023-02-22 16:08:40 +0900

    fix(calendar): Dialog wapl-ui 사용하는 방식으로 전체 수정

--- src/common/components/dialog/Dialog.style.tsx
+++ src/common/components/dialog/Dialog.style.tsx
@@ -1,49 +1,46 @@
-import { styled, TextField, Mui } from '@wapl/ui';
+import { styled, TextField, AlertTitle, AlertSubText, DialogContent, AlertSubAction, Mui } from '@wapl/ui';
 
-export const Title = styled.div`
-  display: flex;
-  font-size: 16px;
-  font-weight: 700;
-  padding: 30px 32px 10px 32px;
-  justify-content: center;
+export const Title = styled(AlertTitle)`
+  margin: 0;
+  ${({ theme: { Font } }) => Font.Text.l.Bold};
 `;
 
-export const InputDialogTitle = styled(Title)`
-  padding: 21px 20px 15px 20px;
+export const SubTitle = styled(AlertSubText)`
+  margin: 0;
+  white-space: pre-line;
+  ${({ theme: { Font } }) => Font.Text.m.Regular};
+  color: ${({ theme: { Color } }) => Color.Gray[900]};
 `;
 
-export const CloseButton = styled(Mui.IconButton)`
-  padding: 0px;
-  margin-left: auto;
+export const Description = styled(AlertSubText)`
+  margin: 0;
+  padding-top: 12px;
+  white-space: pre-line;
+  ${({ theme: { Font } }) => Font.Text.m.Regular};
+`;
+
+export const InputDialogContent = styled(DialogContent)`
+  overflow: hidden;
 `;
 
 export const Input = styled(TextField)`
   height: 32px;
-  margin: 22px 20px 11px 20px;
+  ${({ theme: { Font } }) => Font.Text.m.Regular};
 `;
 
-export const SubTitle = styled.div`
-  display: flex;
-  justify-content: center;
-  font-size: 14px;
-  margin: 0px 32px;
-  white-space: pre-line;
+export const InputDialogDescription = styled(AlertSubText)`
+  margin: 0;
+  ${({ theme: { Font } }) => Font.Text.xs.Regular};
 `;
 
-export const Description = styled.div`
-  display: flex;
-  justify-content: center;
-  text-align: center;
-  white-space: pre-line;
-  font-size: 14px;
-  color: #80868b;
-  margin: 16px 32px 32px;
+export const SelectDialogContent = styled(AlertSubAction)`
+  text-align: start;
 `;
 
-export const InputDialogDescription = styled.div`
-  font-size: 12px;
-  color: #80868b;
-  margin: 0px 14px 22px 20px;
+export const SelectItemLabel = styled(Mui.FormControlLabel)`
+  .MuiTypography-root {
+    ${({ theme: { Font } }) => Font.Text.m.Regular};
+  }
 `;
 
 export const DialogButtonWrapper = styled.div`
@@ -52,5 +49,6 @@ export const DialogButtonWrapper = styled.div`
   padding: 0px 20px 20px 20px;
   button {
     flex: 1;
+    cursor: pointer;
   }
 `;
--- src/common/components/dialog/Dialog.tsx
+++ src/common/components/dialog/Dialog.tsx
@@ -1,4 +1,4 @@
-import { Dialog as DialogCompo, Button } from '@wapl/ui';
+import { Dialog as DialogCompo, AlertWrapper, Button } from '@wapl/ui';
 import { useCalendarStores } from '@/stores/StoreProvider';
 import { Title, SubTitle, Description, DialogButtonWrapper } from './Dialog.style';
 import { InputDialog } from './InputDialog';
@@ -33,19 +33,19 @@ export const Dialog = () => {
       case 'repeatEventUpdate':
         return {
           title: '일정 수정',
-          subTitle: `이 일정은 반복 설정된 일정입니다. \n 선택한 일정을 수정하시겠습니까?`,
+          subTitle: `이 일정은 반복 설정된 일정입니다.\n 선택한 일정을 수정하시겠습니까?`,
         };
       case 'repeatEventDelete':
         return {
           title: '일정 삭제',
-          subTitle: `이 일정은 반복 설정된 일정입니다. \n 선택한 일정을 삭제하시겠습니까?`,
-          description: `공유한 상대방에게도 삭제되며, \n 삭제 후 복구할 수 없습니다.`,
+          subTitle: `이 일정은 반복 설정된 일정입니다.\n 선택한 일정을 삭제하시겠습니까?`,
+          description: `공유한 상대방에게도 삭제되며,\n 삭제 후 복구할 수 없습니다.`,
         };
       case 'calendarDelete':
         return {
           title: '캘린더 삭제',
           subTitle: `선택한 캘린더를 삭제하시겠습니까?`,
-          description: `삭제 후에는 복구할 수 없으며, \n공유한 상대방에게도 사라집니다.`,
+          description: `삭제 후에는 복구할 수 없으며,\n공유한 상대방에게도 사라집니다.`,
         };
       case 'subscriptionDelete':
         return {
@@ -68,7 +68,8 @@ export const Dialog = () => {
           title: '구독 캘린더 추가 실패',
           description: `이미 구독 중입니다.`,
         };
-
+      case 'refresh':
+        return { subTitle: '변경 사항을 저장하지 않고 나가시겠습니까?' };
       default:
         return {};
     }
@@ -97,13 +98,17 @@ export const Dialog = () => {
         ];
       case 'subscribeFail':
       case 'subscribeDuplication':
-        return [{ variant: 'secondary', text: '확인', onClick: onClick[0] }];
+        return [{ variant: 'primary', text: '확인', onClick: onClick[0] }];
       case 'repeatEventUpdate':
         return [
           { variant: 'secondary', text: '취소', onClick: onClick[0] },
           { variant: 'primary', text: '수정', onClick: onClick[1] },
         ];
-
+      case 'refresh':
+        return [
+          { variant: 'secondary', text: '취소', onClick: onClick[0] },
+          { variant: 'primary', text: '나가기', onClick: onClick[1] },
+        ];
       default:
         return [];
     }
@@ -130,9 +135,11 @@ export const Dialog = () => {
       default:
         return (
           <DialogCompo open onClose={handleClose}>
-            <Title>{title?.title}</Title>
-            <SubTitle>{title?.subTitle}</SubTitle>
-            <Description>{title?.description}</Description>
+            <AlertWrapper>
+              <Title>{title?.title}</Title>
+              <SubTitle>{title?.subTitle}</SubTitle>
+              <Description>{title?.description}</Description>
+            </AlertWrapper>
             <DialogButtonWrapper>
               {buttons?.map((button: DialogButton) => (
                 <Button key={button.text} variant={button.variant} onClick={() => button.onClick()}>
