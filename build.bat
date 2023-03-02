2a2fe9864eed35259c4e21917f9e5efc0fd1aece
soohyun <soohyun_kim2@tmax.co.kr>
2023-03-02 13:38:44 +0900

    feat(calendar): yarn build 후 build 파일 zip으로 압축하는 bat 스크립트 적용

---
+++ build.bat
@@ -0,0 +1,42 @@
+@ECHO OFF
+MODE CON COLS=80 LINES=20
+
+ECHO ========================================
+ECHO.
+ECHO     [[ Build dist folder to zip ]]
+ECHO            Author : soohyun
+ECHO. 
+ECHO ========================================
+
+cd "%~dp0"
+REM From folder path
+SET DIST_FOLDER=dist
+REM to folder path
+SET BUILD_FOLDER=build
+REM compress file name 
+SET MODULE_NAME=calendar
+
+REM get _MY_DATETIME
+CALL :SETDATETIME
+SET MODULE_NAME="%MODULE_NAME%_%_TODAY%.zip"
+
+REM bandizip compress
+bandizip.exe c %BUILD_FOLDER%\%MODULE_NAME% %DIST_FOLDER%
+
+ECHO.
+ECHO        Done.
+ECHO. 
+ECHO ========================================
+
+REM close
+GOTO :EOF
+
+REM =============================
+REM functional
+:SETDATETIME
+SET _DATE_FORMAT=%DATE%
+REM  result ->2023-03-02
+SET _DATE_FORMAT=%_DATE_FORMAT:-=%
+REM  result ->20230302
+SET _TODAY=%_DATE_FORMAT%
+REM =============================
