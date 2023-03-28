96e1b8e0dd6ba98a42d98f7e7f93203f08753fc6
soohyun <soohyun_kim2@tmax.co.kr>
2023-03-28 11:31:57 +0900

    chore(calendar): 참여구성원 추가 되었을 때 스크롤 추가 및 스타일 수정

--- src/common/components/EventInfoItem/Participants.style.ts
+++ src/common/components/EventInfoItem/Participants.style.ts
@@ -21,7 +21,7 @@ export const AccordionDetails = styled(Mui.AccordionDetails, {
   min-height: ${({ isExist }) => (isExist ? '28px' : '32px')};
   flex-wrap: wrap;
   align-items: center;
-  padding: ${({ editable, isExist }) => (editable ? (isExist ? '2px' : 0) : '0 0 0 28px')};
+  padding: ${({ editable, isExist }) => (editable ? (isExist ? '2px' : '0 9px') : '0 0 0 28px')};
   margin-bottom: ${({ editable }) => (editable ? '10px' : '8px')};
   background: ${({ editable, theme: { Color } }) => (editable ? Color.Gray[100] : 'none')};
   border-radius: 8px;
@@ -37,7 +37,7 @@ export const ParticipantChip = styled(Chip, {
 `;
 
 export const ParticipantsPlaceholder = styled.span`
-  margin: 0 12px;
   color: ${({ theme: { Color } }) => Color.Gray[400]};
+  ${({ theme: { Font } }) => Font.Text.m.Regular};
   cursor: pointer;
 `;
