8d23a0551f74d18235d2f00ad157825e31814c54
Daeun Oh <daeun_oh@tmax.co.kr>
2023-04-10 15:56:51 +0900

    feat(calendar): 일정 검색 결과 컴포넌트 추가 및 검색바에 검색 서비스 연동
    (cherry picked from commit 6fb2fbf2f9ab9a29ad593056082d9c9f3fb100f1)

---
+++ src/web/components/EventSearchView.style.ts
@@ -0,0 +1,11 @@
+import { styled } from '@wapl/ui';
+
+export const EventSearchViewContainer = styled.div`
+  width: 100%;
+  position: relative;
+  overflow-y: auto;
+`;
+
+export const EventListWrapper = styled.div`
+  padding: 0 18px;
+`;
--- src/web/components/header/EventHeader.tsx
+++ src/web/components/header/EventHeader.tsx
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
+import { autorun } from 'mobx';
 import { useCalendarStores } from '@/stores/StoreProvider';
 import { EventHeaderContainer, EventCreateButton, SearchField } from './EventHeader.style';
 
 const EventHeader: React.FC = () => {
-  const { uiStore } = useCalendarStores();
+  const [keyword, setKeyword] = useState('');
+  const { uiStore, eventStore } = useCalendarStores();
   const navigate = useNavigate();
   const { pathname } = useLocation();
+  const searchFieldRef = useRef(null);
 
   const handleCreateClick = () => {
     navigate(`view-mode/${uiStore.viewMode}/create`, {
@@ -16,12 +19,39 @@ const EventHeader: React.FC = () => {
     });
   };
 
+  const handleSearch = (value: string) => {
+    if (!value.trim()) return;
+    eventStore.setSearchKeyword(value);
+    navigate(`view-mode/${uiStore.viewMode}/search`);
+  };
+
+  useEffect(() => {
+    const dispose = autorun(() => {
+      if (eventStore.searchKeyword) return;
+      setKeyword('');
+      (document.querySelector('.MuiFilledInput-input') as HTMLElement)?.blur();
+    });
+    return () => dispose();
+  }, []);
+
   return (
     <EventHeaderContainer>
       <EventCreateButton onClick={handleCreateClick}>새 일정</EventCreateButton>
 
       {/* <SearchBar type="search" placeholder="캘린더 일정 검색" /> */}
-      <SearchField style={{ visibility: 'hidden' }} placeholder="캘린더 일정 검색" variant="filled" width={280} />
+      <SearchField
+        ref={searchFieldRef}
+        placeholder="캘린더 일정 검색"
+        variant="filled"
+        width={280}
+        value={keyword}
+        onChange={e => setKeyword(e.target.value)}
+        onClear={() => setKeyword('')}
+        onSearch={handleSearch}
+        onKeyPress={e => {
+          if (e.code === 'Enter') handleSearch(keyword);
+        }}
+      />
     </EventHeaderContainer>
   );
 };
