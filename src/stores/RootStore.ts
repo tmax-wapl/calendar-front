d82e58626cdbe612cf2c1f42adf6ad9c4e766e8a
hyojeong_kim <hyojeong_kim@tmax.co.kr>
2023-04-12 13:38:51 +0900

    feat(calendar): 파일 관련 repo, store, interface 추가
    (cherry picked from commit f868f9b8c7da8ffcd275b9e38d0c8053a2230c27)

--- src/stores/RootStore.ts
+++ src/stores/RootStore.ts
@@ -1,15 +1,18 @@
 import CalendarStore from './CalendarStore';
 import EventStore from './EventStore';
 import UiStore from './UiStore';
+import { FileStore } from './FileStore';
 
 export default class RootStore {
   calendarStore: CalendarStore;
   eventStore: EventStore;
   uiStore: UiStore;
+  fileStore: FileStore;
 
   constructor() {
     this.eventStore = new EventStore(this);
     this.uiStore = new UiStore(this);
     this.calendarStore = new CalendarStore(this);
+    this.fileStore = new FileStore(this);
   }
 }
