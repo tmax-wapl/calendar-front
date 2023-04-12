4e11515797eff196b8f4895558f599e50148caa3
hyojeong_kim <hyojeong_kim@tmax.co.kr>
2023-04-12 19:39:32 +0900

    fix(calendar): docs url 추가
    (cherry picked from commit bb46b0448e4b270a953bf18d29b589727fe2ae83)

--- src/common/lib/API.ts
+++ src/common/lib/API.ts
@@ -7,6 +7,8 @@ export const baseUrl = isDevelop
   ? `${window.parent.env?.REACT_APP_CALENDAR_API_URL ?? 'https://calendar.teespace.net'}`
   : `${window.parent.env?.REACT_APP_CALENDAR_API_URL}`;
 
+export const docsUrlPath = process.env?.REACT_APP_OFFICE_API_URL ?? 'http://220.90.208.70:8080/superoffice';
+
 export const SettingInstance = (() => {
   CoreAPI.instance.interceptors.response.use(
     // 동일한 instance 공유하다보니 custom 수정하면 공통 서비스 콜에서 에러
