82de928fdd5e2ebe2e28432258ad975135d48ede
soohyun <soohyun_kim2@tmax.co.kr>
2023-04-13 17:01:18 +0900

    fix(calendar): 파일 첨부 size 영역 깨짐 현상 수정
    (cherry picked from commit 13ef187ca750f80927c35f7490956dd5a91c79fe)

--- src/common/components/EventInfoItem/Attachments.tsx
+++ src/common/components/EventInfoItem/Attachments.tsx
@@ -123,7 +123,7 @@ const Attachments = ({ attachments = [], onChange, editable = false, isUploading
       dm = 2,
       sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'],
       i = Math.floor(Math.log(bytes) / Math.log(k));
-    return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+    return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + sizes[i];
   };
 
   return (
