2993410659580b460a2296ad48bdccef6e9a1ec0
soohyun <soohyun_kim2@tmax.co.kr>
2023-04-18 17:42:27 +0900

    fix(calendar): 일정 공유시 RoomModel & SearchOrgRes & GetFavoriteOrgRes DTO 정의 누락 추가
    (cherry picked from commit 4ff1111d90db7e6b57c54fdcf1949981cd463fe3)

--- src/common/components/ContextMenu/ContextMenuItem.tsx
+++ src/common/components/ContextMenu/ContextMenuItem.tsx
@@ -199,6 +199,25 @@ export const ContextMenuItem = ({ id, type, date, onClose }: Props) => {
     eventStore.event.endDate = toLuxon(date.enddate);
   };
 
+  const isRoomModel = (roomItem: RoomModel): roomItem is RoomModel => {
+    return 'displayName' in roomItem;
+  };
+
+  const isSearchOrgRes = (searchOrgItem: SearchOrgRes): searchOrgItem is SearchOrgRes => {
+    return !('orgId' in searchOrgItem);
+  };
+
+  const convertRoomObj = (item: Partial<RoomModel & SearchOrgRes & GetFavoriteOrgRes>) => {
+    switch (true) {
+      case isRoomModel(item as RoomModel):
+        return item?.id;
+      case isSearchOrgRes(item as SearchOrgRes):
+        return item?.org.roomId;
+      default:
+        return item.roomId;
+    }
+  };
+
   const handleEventShare = async (
     personaIdList: Partial<Member>[],
     roomIdList: Partial<RoomModel & SearchOrgRes & GetFavoriteOrgRes>[],
@@ -206,9 +225,8 @@ export const ContextMenuItem = ({ id, type, date, onClose }: Props) => {
     await eventStore.shareEvent({
       eventId: id,
       personaIdList: personaIdList.map(persona => persona.personaId),
-      roomIdList: roomIdList.map(room => room.id),
+      roomIdList: roomIdList.map(room => convertRoomObj(room)),
     });
-    // TODO: 공유 완료 되었다는 팝업
   };
 
   const handleEventShareClick = () => {
