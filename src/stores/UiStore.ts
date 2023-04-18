d966e9de9839d92c01aa09e5c9ecb537426dfb80
soohyun <soohyun_kim2@tmax.co.kr>
2023-04-18 16:52:57 +0900

    fix(calendar): 같은 달 조건 제거, datePicker 선택시 전환 되도록 수정

--- src/stores/UiStore.ts
+++ src/stores/UiStore.ts
@@ -111,23 +111,21 @@ export default class UiStore {
     return this.mainApi;
   }
 
-  changeDateRange() {
+  changeDateRange(selectDate?: Date) {
     this.setDateRange({
       start: toDateString(this.mainApi.view.activeStart),
-      view: DateTime.fromJSDate(this.mainApi?.getDate()),
+      view: DateTime.fromJSDate(!selectDate ? this.mainApi?.getDate() : selectDate),
       end: toDateString(this.mainApi.view.activeEnd),
     });
   }
 
   handleDateClick(date: DateTime, callback?: () => void) {
     if (!this.mainApi) return;
-    // 같은 달 내에서 선택 할 때 main calendar render 안하도록.
-    if (isEqualMonth(this.mainApi?.getDate(), date?.toJSDate())) {
-      if (callback) callback();
-      return;
-    }
+
     this.mainApi.changeView(this.mainApi.view.type, date.toJSDate());
-    this.changeDateRange();
+    this.changeDateRange(date.toJSDate());
+    this.setDateDay(date);
+
     if (callback) callback();
   }
 }
