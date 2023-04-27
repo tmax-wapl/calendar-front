e56b5b52c59954a826edfea7fe30d601d21bb610
soohyun <soohyun_kim2@tmax.co.kr>
2023-04-27 17:24:37 +0900

    fix(calendar): mainFlag 원복 및 delete시 fetch되도록 추가

--- src/web/components/layout/RoomCalendarItem.tsx
+++ src/web/components/layout/RoomCalendarItem.tsx
@@ -58,7 +58,7 @@ const RoomCalendarItem = observer(({ calendar }: Props) => {
   };
 
   return (
-    <ItemContainer main={false}>
+    <ItemContainer main={calendar.mainFlag}>
       {calendarStore.renameId === calendar.roomId ? (
         <InputItemContainer calendarcolor={calendar.color}>
           <Checkbox checked={calendar.checkFlag} />
