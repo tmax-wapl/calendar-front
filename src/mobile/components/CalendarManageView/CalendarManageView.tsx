8a30125f1d1c55992db58be22b2d09703a2e5306
hyojeong_kim <hyojeong_kim@tmax.co.kr>
2023-05-10 10:43:17 +0900

    feat(calendar): 캘린더 관리화면 스크롤 추가

--- src/mobile/components/CalendarManageView/CalendarManageView.tsx
+++ src/mobile/components/CalendarManageView/CalendarManageView.tsx
@@ -2,7 +2,7 @@ import { useCalendarStores } from '@/stores/StoreProvider';
 import FilterList from './FilterList';
 import MyCalendarList from './MyCalendarList';
 import OtherCalendarList from './OtherCalendarList';
-import { CalendarManageViewContainer, Divider } from './CalendarManageView.style';
+import { CalendarManageViewContainer, ContentContainer, Divider } from './CalendarManageView.style';
 import EventBar from '../header/EventBar';
 
 const CalendarManageView = () => {
@@ -13,16 +13,16 @@ const CalendarManageView = () => {
   };
 
   return (
-    <>
+    <CalendarManageViewContainer>
       <EventBar title="캘린더 관리" leftSide={[{ action: 'close', onClick: handleBackClick }]} />
-      <CalendarManageViewContainer>
+      <ContentContainer>
         <FilterList />
         <Divider />
         <MyCalendarList />
         <Divider />
         <OtherCalendarList />
-      </CalendarManageViewContainer>
-    </>
+      </ContentContainer>
+    </CalendarManageViewContainer>
   );
 };
 
