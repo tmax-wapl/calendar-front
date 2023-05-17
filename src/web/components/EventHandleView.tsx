31a8017c88328fd27a181a5787991ccee78f2591
hyojeong_kim <hyojeong_kim@tmax.co.kr>
2023-05-17 14:03:50 +0900

    fix(calendar): 파일 수정 시, 기존에 첨부되었던 파일이 없어지는 이슈
    (cherry picked from commit 1aae3a8fa5380cf68df33ec03f9d531fe15b1591)

--- src/web/components/EventHandleView.tsx
+++ src/web/components/EventHandleView.tsx
@@ -117,6 +117,9 @@ const EventHandleView = ({ action }: Props) => {
   };
 
   const preprocessFile = async () => {
+    const originFileList = originEvent.attachments.map(originFile =>
+      eventStore.event.attachments.some(file => file.docsFileId === originFile.docsFileId) ? originFile : {},
+    );
     const uploadPromiseList = eventStore.fileList.map(file => {
       return uploadFile(file);
     });
@@ -128,7 +131,7 @@ const EventHandleView = ({ action }: Props) => {
     // );
     const res = await Promise.all(uploadPromiseList.map(promise => promise.catch(err => null)));
     eventStore.setFileList([]);
-    return res.filter(file => file !== null);
+    return [...res, ...originFileList].filter(file => file !== null);
   };
 
   const preprocessEvent = async (event: EventModel): Promise<EventModel> => {
