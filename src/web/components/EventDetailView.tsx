9d94e7deda56ed4f77e85a1afaa49ffab16d0d0c
hyojeong_kim <hyojeong_kim@tmax.co.kr>
2023-05-17 14:06:00 +0900

    fix(calendar): 일정 생성/수정 중 강제 취소 시, 파일 데이터가 남아있고 일정 상세보기에 로딩화면이 남아있는 이슈

--- src/web/components/EventDetailView.tsx
+++ src/web/components/EventDetailView.tsx
@@ -20,12 +20,13 @@ import { EVENT_UPDATE_OPTION } from '@common/constants';
 
 interface OutletProps {
   isEventUpdating: boolean;
+  setEventUpdating: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 const EventDetailView = () => {
   const { calendarStore, eventStore, uiStore, fileStore } = useCalendarStores();
   const navigate = useNavigate();
-  const { isEventUpdating } = useOutletContext<OutletProps>();
+  const { isEventUpdating, setEventUpdating } = useOutletContext<OutletProps>();
 
   const handleBackClick = () => {
     navigate(`/main/view-mode/${uiStore.viewMode}/date`);
@@ -137,6 +138,8 @@ const EventDetailView = () => {
     Array.from(fileStore.uploadInfo.values()).map(info => info.cancelSource.cancel());
     e.preventDefault();
     e.returnValue = '';
+    eventStore.setFileList([]);
+    setEventUpdating(false);
     navigate(`/main/view-mode/${uiStore.viewMode}/date`);
   };
 
