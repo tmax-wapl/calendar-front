0d71dd59afd43910f14a7a0e27ba54703c416ea3
Daeun Oh <daeun_oh@tmax.co.kr>
2023-05-19 17:13:48 +0900

    chore(calendar): 미리 알림 컴포넌트 추가

--- src/web/components/EventHandleView.tsx
+++ src/web/components/EventHandleView.tsx
@@ -270,6 +270,7 @@ const EventHandleView = ({ action }: Props) => {
           calId: calId,
           start: toISO(start),
           end: toISO(start.plus({ minutes: 30 })),
+          alarmList: ['0'],
         }),
       );
       setOriginEvent(new EventModel({ ...eventStore.event.dto }));
@@ -440,7 +441,7 @@ const EventHandleView = ({ action }: Props) => {
             />
           )}
         </Observer>
-        {/* <Observer>
+        <Observer>
           {() => (
             <Notifications
               notifications={eventStore.event.notifications}
@@ -448,7 +449,7 @@ const EventHandleView = ({ action }: Props) => {
               editable
             />
           )}
-        </Observer> */}
+        </Observer>
         <Observer>
           {() => (
             <Description
