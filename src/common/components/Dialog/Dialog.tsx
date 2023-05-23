660a74f500680221eefa5eaa224313c88aabfc2b
Daeun Oh <daeun_oh@tmax.co.kr>
2023-05-23 11:02:06 +0900

    fix(calendar): 참여 구성원 다이얼로그 탭에 즐겨찾기 추가

--- src/common/components/Dialog/Dialog.tsx
+++ src/common/components/Dialog/Dialog.tsx
@@ -175,7 +175,7 @@ export const Dialog = () => {
             open
             onClose={onCloseClick}
             title={data?.title}
-            tabs={['org', 'room']}
+            tabs={['favorite', 'org', 'room']}
             confirmBtnText={'공유'}
             onComplete={onComplete}
             chipListText={'공유할 멤버를 선택하세요.\n 공유 최대 멤버는 200명입니다.'}
@@ -187,7 +187,7 @@ export const Dialog = () => {
             open
             onClose={onCloseClick}
             title={data?.title}
-            tabs={['org', 'room']}
+            tabs={['favorite', 'org', 'room']}
             confirmBtnText={'공유'}
             onComplete={onComplete}
           />
