67ae2455272f7c721bfccfdca61a98bd07137215
hyojeong_kim <hyojeong_kim@tmax.co.kr>
2023-05-24 14:37:00 +0900

    fix(calendar): 일정 상세보기 중 해당 일정이 있는 캘린더 컬러 변경 시, 상세보기에도 컬러 반영되도록 수정
    (cherry picked from commit b055c95d941b7dcea5f488798c501d752f885b21)

--- src/common/components/ContextMenu/ContextMenu.tsx
+++ src/common/components/ContextMenu/ContextMenu.tsx
@@ -72,6 +72,7 @@ export const ContextMenu = () => {
           event.roomId === id ? new EventModel({ ...event.dto, calColor: color }) : event,
         );
         calendarStore.setEventList(roomEventList);
+        if (pathname.includes('detail')) eventStore.setEvent(new EventModel({ ...eventStore.event.dto, calColor }));
         uiStore.setContextClickArg({ ...uiStore.contextClickArg, color });
         break;
       default:
