eca6f442f00b5753ae7919d21d77d762e60d988a
hyojeong_kim <hyojeong_kim@tmax.co.kr>
2023-05-26 15:16:39 +0900

    fix(calendar): 모바일 캘린더 관리 화면에 캘린더 색상 없는 경우 페르소나 컬러 적용

--- src/mobile/components/CalendarManageView/CalendarSettingView.tsx
+++ src/mobile/components/CalendarManageView/CalendarSettingView.tsx
@@ -85,7 +85,7 @@ const CalendarSettingView = observer(() => {
       calendarStore.setEventList(roomEventList);
     } else {
       await calendarStore.updateCalendar(id, { color });
-      calendarStore.updateCalendarDTO(id, 'color', color);
+      calendarStore.updateCalendarDTO(id, 'color', calColor);
       if (type === 'share') {
         const sharedEventList = calendarStore.eventList.map(event =>
           event.roomId === null && event.shareEvent ? new EventModel({ ...event.dto, calColor }) : event,
