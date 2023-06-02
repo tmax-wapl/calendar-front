bd5b1c9f914fd09ed75a39f754b5d0fb0b43cc80
Daeun Oh <daeun_oh@tmax.co.kr>
2023-06-02 17:39:09 +0900

    feat(calendar): Swiper를 활용한 WheelPicker 컴포넌트
    (cherry picked from commit 772e0e46f8a8ebdeccb3f20763848b1f95583fae)

---
+++ src/common/components/WheelPicker/WheelPicker.style.ts
@@ -0,0 +1,8 @@
+import { styled } from '@wapl/ui';
+import { SwiperSlide } from 'swiper/react';
+
+export const StyledSwiperSlide = styled(SwiperSlide)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
---
+++ src/common/components/WheelPicker/WheelPicker.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import { Swiper } from 'swiper/react';
+import 'swiper/css';
+import { StyledSwiperSlide } from './WheelPicker.style';
+
+interface Props {
+  visibleHeight: number;
+  slides: string[];
+  slidesPerView: number;
+  selectedIndex: number;
+  surroundingBackup?: number;
+  onValueChange?: (index: number) => void;
+  loop?: boolean;
+}
+
+const WheelPicker = ({
+  visibleHeight,
+  slides,
+  slidesPerView,
+  selectedIndex,
+  surroundingBackup,
+  onValueChange,
+  loop,
+}: Props) => {
+  return (
+    <Swiper
+      style={{ width: '100%', height: visibleHeight }}
+      slidesPerView={slidesPerView}
+      loop={loop}
+      direction={'vertical'}
+      initialSlide={selectedIndex}
+      centeredSlides={true}
+      onSlideChangeTransitionEnd={swiper => {
+        if (swiper.realIndex % slides.length === selectedIndex) return;
+        onValueChange?.(swiper.realIndex % slides.length);
+      }}
+    >
+      {[...Array(surroundingBackup ?? 0)].map((_, index) => (
+        <React.Fragment key={index}>
+          {slides.map(slide => (
+            <StyledSwiperSlide key={index + slide}>{slide}</StyledSwiperSlide>
+          ))}
+        </React.Fragment>
+      ))}
+      {slides.map(slide => (
+        <StyledSwiperSlide key={slide}>{slide}</StyledSwiperSlide>
+      ))}
+      {[...Array(surroundingBackup ?? 0)].map((_, index) => (
+        <React.Fragment key={surroundingBackup + index}>
+          {slides.map(slide => (
+            <StyledSwiperSlide key={surroundingBackup + index + slide}>{slide}</StyledSwiperSlide>
+          ))}
+        </React.Fragment>
+      ))}
+    </Swiper>
+  );
+};
+
+export default WheelPicker;
