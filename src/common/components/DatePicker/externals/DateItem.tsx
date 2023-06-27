6e67643e8a6773f17aad850107589a8f4fc3f40e
soohyun <soohyun_kim2@tmax.co.kr>
2023-06-27 16:29:28 +0900

    feat: dateItem과 datePicker 으로 배포하도록 분리
    (cherry picked from commit 1b506377d727e3943a15afa74a8d38f7fb6372a8)

---
+++ src/common/components/DatePicker/externals/DateItem.tsx
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import { Icon, Tooltip, WaplUiProvider } from '@wapl/ui';
+import { DateTime } from 'luxon';
+import { PickerContainer, DateWrapper, DatePickerWrapper } from '../../EventInfoItem/EventDateItem.style';
+import DatePicker from '../components/DatePicker';
+
+interface Props {
+  date: Date;
+  isDateInvalid?: boolean;
+  inValidTitle?: string;
+  onChange?: (date: Date) => void;
+}
+
+export const DateItem: React.FC<Props> = ({ date, isDateInvalid, inValidTitle, onChange }: Props) => {
+  const [isDatePickerOpen, setIsDatePickerOpen] = useState<boolean>(false);
+
+  const handleDateClick = () => {
+    setIsDatePickerOpen(prev => !prev);
+  };
+
+  const handleDateChange = (date: DateTime) => {
+    if (onChange) onChange(date.toJSDate());
+  };
+
+  return (
+    <WaplUiProvider>
+      <PickerContainer>
+        <Tooltip
+          disableHoverListener={!isDateInvalid}
+          placement="top"
+          title={inValidTitle ?? '시작일과 같거나 이후로 설정해 주세요.'}
+          sx={{ '.MuiTooltip-tooltip': { maxWidth: '250px' } }}
+        >
+          <DateWrapper
+            className={`${isDatePickerOpen ? 'selected' : ''}`}
+            isInvalid={isDateInvalid}
+            onClick={handleDateClick}
+          >
+            {DateTime.fromJSDate(date).toFormat('yyyy.LL.dd')}
+            <span style={{ display: 'flex', marginLeft: '4px' }}>
+              <Icon.CalendarLine width={16} height={16} />
+            </span>
+          </DateWrapper>
+        </Tooltip>
+        {isDatePickerOpen && (
+          <DatePickerWrapper allDay>
+            <DatePicker
+              date={DateTime.fromJSDate(date)}
+              onDateClick={handleDateChange}
+              onOutsideClick={handleDateClick}
+            />
+          </DatePickerWrapper>
+        )}
+      </PickerContainer>
+    </WaplUiProvider>
+  );
+};
+
+DateItem.displayName = 'DateItem';
--- src/common/components/DatePicker/externals/DatePicker.tsx
+++ src/common/components/DatePicker/externals/DatePicker.tsx
@@ -1,47 +1,31 @@
-import React, { useState } from 'react';
-import { Icon, Tooltip, WaplUiProvider } from '@wapl/ui';
+import React from 'react';
+import { WaplUiProvider } from '@wapl/ui';
 import { DateTime } from 'luxon';
-import { PickerContainer, DateWrapper, DatePickerWrapper } from '../../EventInfoItem/EventDateItem.style';
+import { PickerContainer, DatePickerWrapper } from '../../EventInfoItem/EventDateItem.style';
 import DatePickerCompo from '../components/DatePicker';
 
 interface Props {
-  date: DateTime;
-  allDay?: boolean;
-  isDateInvalid?: boolean;
-  inValidTitle?: string;
-  onChange?: (date: DateTime) => void;
+  open: boolean;
+  date: Date;
+  onChange?: (date: Date) => void;
+  onOutsideClick?: () => void;
 }
 
-export const DatePicker: React.FC<Props> = ({ date, allDay, isDateInvalid, inValidTitle, onChange }: Props) => {
-  const [isDatePickerOpen, setIsDatePickerOpen] = useState<boolean>(false);
-
-  const handleDateClick = () => {
-    setIsDatePickerOpen(prev => !prev);
+export const DatePicker: React.FC<Props> = ({ open, date, onOutsideClick, onChange }: Props) => {
+  const handleDateChange = (date: DateTime) => {
+    if (onChange) onChange(date.toJSDate());
   };
 
   return (
     <WaplUiProvider>
       <PickerContainer>
-        <Tooltip
-          disableHoverListener={!isDateInvalid}
-          placement="top"
-          title={inValidTitle ?? '시작일과 같거나 이후로 설정해 주세요.'}
-          sx={{ '.MuiTooltip-tooltip': { maxWidth: '250px' } }}
-        >
-          <DateWrapper
-            className={`${isDatePickerOpen ? 'selected' : ''}`}
-            isInvalid={isDateInvalid}
-            onClick={handleDateClick}
-          >
-            {date.toFormat('yyyy.LL.dd')}
-            <span style={{ display: 'flex', marginLeft: '4px' }}>
-              <Icon.CalendarLine width={16} height={16} />
-            </span>
-          </DateWrapper>
-        </Tooltip>
-        {isDatePickerOpen && (
-          <DatePickerWrapper allDay={allDay}>
-            <DatePickerCompo date={date} onDateClick={onChange} onOutsideClick={handleDateClick} />
+        {open && (
+          <DatePickerWrapper allDay>
+            <DatePickerCompo
+              date={DateTime.fromJSDate(date)}
+              onDateClick={handleDateChange}
+              onOutsideClick={onOutsideClick}
+            />
           </DatePickerWrapper>
         )}
       </PickerContainer>
