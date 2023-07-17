68248c430f6d2ffa0330a2d0430ee67ce4539cef
soohyun <soohyun_kim2@tmax.co.kr>
2023-07-17 18:28:09 +0900

    fix(web): tsconfig include 수정

--- webpack.config.js
+++ webpack.config.js
@@ -19,8 +19,6 @@ module.exports = env => {
       alias: {
         '@': path.resolve(__dirname, 'src'),
         '@common': path.resolve(__dirname, 'src/common'),
-        '@constants': path.resolve(__dirname, 'src/common/constants'),
-        '@api': path.resolve(__dirname, 'src/common/lib'),
         '@wcomponents': path.resolve(__dirname, 'src/web/components'),
         '@mcomponents': path.resolve(__dirname, 'src/mobile/components'),
         '@contexts': path.resolve(__dirname, 'src/common/contexts'),
